41316f9ff73fa3057773453ae19eca5838cb09c1
xiaoxigua-1 <aijdfajodwsdf@gmail.com>
2021-10-30 23:49:46 +0800

    add table

--- src/components/home/index.tsx
+++ src/components/home/index.tsx
@@ -1,11 +1,29 @@
-import React from 'react';
+import React, {useState, SyntheticEvent} from 'react';
+import Tabs from '@mui/material/Tabs';
+import Tab from '@mui/material/Tab';
+import './home.sass';
 
 /**
  * @return {JSX}
 **/
 function Home() {
+  const [table, setTable] = useState('search');
+  const tableChange = (_: SyntheticEvent, newValue: string) => {
+    setTable(newValue);
+  };
   return (
-    <div id="home"></div>
+    <div id="home">
+      <Tabs
+        value={table}
+        onChange={tableChange}
+        variant="fullWidth"
+        centered
+        id="menu"
+      >
+        <Tab label="搜尋" value="search" sx={{color: '#c5c3c3'}}/>
+        <Tab label="路線規劃" value="directions" sx={{color: '#c5c3c3'}}/>
+      </Tabs>
+    </div>
   );
 }
 
