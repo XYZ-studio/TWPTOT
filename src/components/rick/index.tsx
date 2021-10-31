49358539b895e7d8e25ba53b56f26fe39fe62043
xiaoxigua-1 <aijdfajodwsdf@gmail.com>
2021-10-31 19:15:15 +0800

    rick textt

--- src/components/rick/index.tsx
+++ src/components/rick/index.tsx
@@ -6,7 +6,10 @@ import './rick.sass';
 **/
 function Rick() {
   return (
-    <iframe id="rick" src="https://www.youtube-nocookie.com/embed/dQw4w9WgXcQ?rel=0&autoplay=1&loop=1&playlist=dQw4w9WgXcQ" title="YouTube video player" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" ></iframe>
+    <div>
+      <iframe id="rick" src="https://www.youtube-nocookie.com/embed/dQw4w9WgXcQ?rel=0&autoplay=1&loop=1&playlist=dQw4w9WgXcQ" title="YouTube video player" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" ></iframe>
+      <h1 style={{margin: 0, textAlign: 'center'}}>你被Rick了</h1>
+    </div>
   );
 }
 
