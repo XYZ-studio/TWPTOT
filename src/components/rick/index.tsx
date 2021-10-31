254a8b843da6713d0b92e26c4fe1d6fcbb48241e
xiaoxigua-1 <aijdfajodwsdf@gmail.com>
2021-10-31 19:17:13 +0800

    rick text color

--- src/components/rick/index.tsx
+++ src/components/rick/index.tsx
@@ -8,7 +8,7 @@ function Rick() {
   return (
     <div>
       <iframe id="rick" src="https://www.youtube-nocookie.com/embed/dQw4w9WgXcQ?rel=0&autoplay=1&loop=1&playlist=dQw4w9WgXcQ" title="YouTube video player" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" ></iframe>
-      <h1 style={{margin: 0, textAlign: 'center'}}>你被Rick了</h1>
+      <h1 style={{margin: 0, textAlign: 'center', color: '#ffff'}}>你被Rick了</h1>
     </div>
   );
 }
