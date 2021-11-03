9d70ae7a8ef35bc74211ecbbc8b8b58f51e42631
xiao xigua <aijdfajodwsdf@gmail.com>
2021-11-03 23:30:52 +0800

    add select stations component

---
+++ src/components/home/railway/selectStation/index.tsx
@@ -0,0 +1,64 @@
+import React from 'react';
+import InputLabel from '@mui/material/InputLabel';
+import MenuItem from '@mui/material/MenuItem';
+import FormControl from '@mui/material/FormControl';
+import Select, {SelectChangeEvent} from '@mui/material/Select';
+import {CityListType} from '../../data/City';
+import {Station} from '../../data/Station';
+
+interface SelectStation {
+  selectCity: CityListType;
+  selectStation: string;
+  railwayStations: Array<Station>;
+  handleChange: (event: SelectChangeEvent) => void;
+}
+/**
+ * @return {JSX.Element}
+**/
+function SelectStation(
+    {
+      selectCity,
+      railwayStations,
+      selectStation,
+      handleChange,
+    }: SelectStation,
+): JSX.Element {
+  return (
+    <FormControl style={{width: '150px'}}>
+      <InputLabel
+        id="demo-simple-select-label"
+        style={{color: '#ffff'}}
+      >車站</InputLabel>
+      <Select
+        labelId="demo-simple-select-label"
+        id="demo-simple-select"
+        value={selectStation}
+        label="車站"
+        onChange={handleChange}
+        sx={{color: '#c5c3c3'}}
+      >
+        {
+          railwayStations.map((station: Station) => {
+            return station.LocationCityCode === '' ? (
+              <MenuItem
+                value={station.StationName.En}
+                key={station.StationName.En}
+              >
+                {station.StationName.Zh_tw}
+              </MenuItem>
+            ) : station.LocationCityCode === selectCity ? (
+              <MenuItem
+                value={station.StationName.En}
+                key={station.StationName.En}
+              >
+                {station.StationName.Zh_tw}
+              </MenuItem>
+            ) : null;
+          })
+        }
+      </Select>
+    </FormControl>
+  );
+}
+
+export default SelectStation;
