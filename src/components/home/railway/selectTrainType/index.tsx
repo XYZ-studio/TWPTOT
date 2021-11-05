98eac312cd860736566d533b84bc771e49c3b82e
zanzan <tea915656@gmail.com>
2021-11-05 10:40:32 +0800

    add select train type

---
+++ src/components/home/railway/selectTrainType/index.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+import InputLabel from '@mui/material/InputLabel';
+
+/**
+ * @return {JSX.Element}
+**/
+
+function SelectTrainType(): JSX.Element{
+  return (
+    <div></div>
+  );
+}
+
+export default SelectTrainType;
