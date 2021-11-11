a65d24aa2ca88a6b53ba6fba6beac460e82d9ab0
xiaoxigua-1 <aijdfajodwsdf@gmail.com>
2021-11-11 11:00:10 +0800

    add ptx api使用聲明

--- src/components/home/contactus/index.tsx
+++ src/components/home/contactus/index.tsx
@@ -18,7 +18,8 @@ function Contactus(): JSX.Element {
       <div className='card'>
         <img src='https://avatars.githubusercontent.com/u/77784793?v=4' />
       </div>
-      <h1></h1>
+      <h1>使用交通部PTX服務平臺</h1>
+      <img src="https://ptx.transportdata.tw/PTX/logo.jpg" width="300px"/>
     </div>
   );
 }
