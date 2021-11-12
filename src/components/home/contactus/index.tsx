e67fdb016da45f69ce260492deb79cc0b20d6b85
Young <young20050727@gmail.com>
2021-11-12 09:44:28 +0800

    Update index.tsx

--- src/components/home/contactus/index.tsx
+++ src/components/home/contactus/index.tsx
@@ -10,23 +10,32 @@ function Contactus(): JSX.Element {
       <h1>Team XYZ</h1>
       <h2>我們是這個網站的開發團隊</h2>
       <div id='member'>
-        <div>
+        <div className='info'>
           <div className='card'>
             <img src='https://avatars.githubusercontent.com/u/62501690?v=4' />
           </div>
           <h3 className='dcid'>Young#0001</h3>
+          <a className="fab fa-github" href="https://github.com/Young-TW"></a>
+          <a className="fab fa-facebook" href="https://www.facebook.com/young20050727"></a>
+          <a className="fab fa-instagram" href="https://www.instagram.com/young__tw"></a>
         </div>
-        <div>
+        <div className='info'>
           <div className='card'>
             <img src='https://avatars.githubusercontent.com/u/60529600?v=4' />
           </div>
           <h3 className='dcid'>xiao xigua#8787</h3>
+          <a className="fab fa-github" href="https://github.com/xiaoxigua-1"></a>
+          <a className="fab fa-facebook" href="https://www.facebook.com/87408740A"></a>
+          <a className="fab fa-instagram" href="https://www.instagram.com/xiaoxigua612/"></a>
         </div>
-        <div>
+        <div className='info'>
           <div className='card'>
             <img src='https://avatars.githubusercontent.com/u/77784793?v=4' />
           </div>
           <h3 className='dcid'>Zanzan (Space Master)#6122</h3>
+          <a className="fab fa-github" href="https://github.com/zanya945"></a>
+          <a className="fab fa-facebook" href="https://www.facebook.com/profile.php?id=100002851670535"></a>
+          <a className="fab fa-instagram" href="https://www.instagram.com/owo_music_game/"></a>
         </div>
       </div>
       <h1 className='title'>使用的API</h1>
